4a4511f10741a98fc3f83bedcf45113aab30c591
thl <984149833@qq.com>
2020-12-11 15:26:42 +0800

    read write lock

--- algorithm/readwritelock.cpp
+++ algorithm/readwritelock.cpp
@@ -3,4 +3,125 @@
 //
 
 #include <iostream>
-https://zhuanlan.zhihu.com/p/86508499
+#include <atomic>
+#include <thread>
+#include <mutex>
+
+//TODO:1.内存模型，指令序列
+//TODO:2，性能优化，空转
+class RWlock
+{
+private:
+    std::atomic<uint32_t> Flag;
+public:
+    RWlock(const RWlock&) = delete;
+    RWlock& operator=(const RWlock&) = delete;
+    RWlock() noexcept : Flag(0) {}
+
+    ///第一个bit分配给写锁，其他bit分配给读锁
+    ///写锁优先级高于读锁
+    void Lockread() noexcept
+    {
+        uint32_t expected = Flag.load() & 0x7fffffff;
+        while(!Flag.compare_exchange_weak(expected,expected+1))
+        {
+            expected &= 0x7fffffff;
+        }
+    }
+    void Unlockread() noexcept
+    {
+        Flag--;
+    }
+
+    void Lockwrite() noexcept
+    {
+        uint32_t expected = Flag.load() & 0x7fffffff;
+        while(!Flag.compare_exchange_weak(expected,expected + 0x80000000))
+        {
+            expected &= 0x7fffffff;
+        }
+        while(Flag.load() & 0x7fffffff != 0)
+        {
+
+        }
+    }
+    void  Unlockwrite() noexcept
+    {
+        Flag -= 0x80000000;
+    }
+};
+
+
+int num = 0;
+RWlock glock;
+std::mutex lock;
+
+void read(int x)
+{
+    int count = 10;
+    while (count--)
+    {
+        glock.Lockread();
+//        lock.lock();
+        printf("%d read  : %d\n",x,num);
+//        lock.unlock();
+        glock.Unlockread();
+    }
+
+}
+
+void write(int x)
+{
+    int count = 10;
+    while (count--)
+    {
+        glock.Lockwrite();
+//        lock.lock();
+        printf("%d write : %d\n",x,++num);
+//        lock.unlock();
+        glock.Unlockwrite();
+    }
+
+}
+
+int main()
+{
+    std::thread t1(read,1);
+    std::thread t2(write,2);
+    std::thread t3(read,3);
+    std::thread t4(write,4);
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///https://zhuanlan.zhihu.com/p/86508499
